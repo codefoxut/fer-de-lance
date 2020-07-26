1c685d6bf7e6caa020cbe409c645694d0b612d4d
Ujjwal Tak <ujjwal.tak@gmail.com>
2020-07-27 02:58:56 +0530

    facade pattern and factory method pattern added.

--- jExercise/design_course/DesignPatterns/src/App.java
+++ jExercise/design_course/DesignPatterns/src/App.java
@@ -1,5 +1,14 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("DESIGN PATTERNS");
+        System.out.println("DESIGN PATTERNS: SINGLETON PATTERN");
+        ExampleSingleton singletonObj1 = ExampleSingleton.getInstance();
+        ExampleSingleton singletonObj2 = ExampleSingleton.getInstance();
+        // ExampleSingleton singletonObj3 = new ExampleSingleton(); cant call it as no constructor exists in class.
+        System.out.println("compare objects "+ singletonObj1.hashCode() + ' '+ singletonObj2.hashCode());
+        System.out.println("value of x = " + singletonObj1.x);
+        
     }
 }
